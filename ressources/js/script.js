6d01db67aeef0db1302cf2fb6ad2c3350be75113
Unknown <damienvignes@hotmail.com>
2018-03-08 10:40:56 +0100

    js + waypoint

--- ressources/js/script.js
+++ ressources/js/script.js
@@ -1,12 +1,27 @@
 $(document).ready(function(){
 
-  $("h1").click(function(){
+  $(".thomas").waypoint(function(direction){
+    if (direction=="down"){
+      $("nav").addClass("sticky");
+    } else {
+      $("nav").removeClass("sticky");
+    }
+  })
+
+
+
+
+
+
+
+
+  /* $("h1").click(function(){
     $(this).css("background-color", "#ff0000");
   })
 
   $("h2").click(function(){
     $(this).css("border", "1px solid red");
-  })
+  }) */
 
 
 });
